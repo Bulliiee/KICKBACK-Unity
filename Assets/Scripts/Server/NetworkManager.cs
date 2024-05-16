43bdd5cd1323c2021f3280eb6c3799ed76601bc4
이항우 <bullie_hu@naver.com>
2024-05-16 13:12:12 +0900

    Feat[Auth]: 작성

--- Assets/Scripts/Server/NetworkManager.cs
+++ Assets/Scripts/Server/NetworkManager.cs
@@ -8,6 +8,7 @@ namespace Highlands.Server
 {
     public enum CurrentPlayerLocation
     {
+        Login,
         Lobby,
         WaitingRoom,
         InGame
@@ -17,14 +18,59 @@ namespace Highlands.Server
     {
         private void Update()
         {
-            UpdateChatLog();
-            UpdateBusinessLog();
+            // 로그인이 아닌 경우 TCP 연결 체킹
+            if (currentPlayerLocation != CurrentPlayerLocation.Login)
+            {
+                UpdateChatLog();
+                UpdateBusinessLog();
+            }
         }
         
         private delegate void UpdateCurrentChattingPlace();
         
         //Todo : GameManager Stanby
-        public CurrentPlayerLocation currentPlayerLocation = CurrentPlayerLocation.Lobby;
+        public CurrentPlayerLocation currentPlayerLocation = CurrentPlayerLocation.Login;
+
+        #region 인증
+
+        private HTTPController _httpController;
+        
+        // Get요청 보내기
+        public void GetRequest<T>(string requestData, string requestUrl, Action<T> resultCallback)
+        {
+            StartCoroutine(_httpController.SendGetRequest(requestData, requestUrl, (result) =>
+            {
+                try
+                {
+                    // 문자열 형태의 JSON을 받은 경우 객체로 변환 후 결과 넘김
+                    T t = JsonUtility.FromJson<T>(result);
+                    resultCallback?.Invoke(t);
+                }
+                catch (Exception e)
+                {
+                    // 파싱 실패, 문자열 형태 숫자인 경우(에러코드)
+                    if (typeof(T) == typeof(string))
+                    {
+                        resultCallback?.Invoke((T)(object)result);
+                    }
+                    else
+                    {
+                        Debug.Log("HTTPController RequestData execute fail");
+                    }
+                }
+            }));
+        }
+    
+        // Post 요청 보내기
+        public void PostRequest<T>(T t, string requestUrl, Action<long> resultCallback)
+        {
+            StartCoroutine(_httpController.SendPostRequest(t, requestUrl, (result) =>
+            {
+                resultCallback?.Invoke(result);
+            }));
+        }
+
+        #endregion
 
         #region 채팅 서버
         
--- Assets/Scripts/Server_/GameManager.cs
+++ Assets/Scripts/Server_/GameManager.cs
@@ -2,14 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using Modules;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
-    void Start()
+    public User user;
+    
+    string[] canvases = {
+        "Login Canvas",
+        "Lobby Canvas",
+        "Channel Canvas"
+    };
+    
+    private void Start()
     {
-        // MasterScene에서 필요한 Manager 생성 후 MainStage 이동
-        LoadingSceneManager.LoadScene("MainStage");
+        // MasterScene에서 필요한 Manager 생성 후 MainStage의 로그인으로 이동
+        ChangeMainStageCanvas("Login Canvas");
     }
 
+    public void ChangeMainStageCanvas(string canvasName)
+    {
+        if (SceneManager.GetActiveScene().name != "MainStage")
+        {
+            LoadingSceneManager.LoadScene("MainStage");
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (canvasName.Equals(canvases[i]))
+            {
+                GameObject.Find(canvasName).SetActive(true);
+            }
+            else
+            {
+                GameObject.Find(canvasName).SetActive(false);
+            }
+        }
+    }
+
+    public void GoToIngame()
+    {
+        
+    }
 
 }
